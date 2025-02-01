4fb16934dc2af966a266bdbf45e9e91449701f98
Emil <emil88pl@gmail.com>
2025-02-01 14:36:30 +0000

    save model in localStorage

--- index.js
+++ index.js
@@ -165,6 +165,7 @@ document.getElementById('send').addEventListener('click', async () => {
 });
 
 function displayChatList() {
+    const modelLlm = document.getElementById('llm').value;
     const previousChatP = document.getElementById('previousChatP');
     previousChatP.innerHTML = ''; // Clear previous list
     const savedChats = JSON.parse(localStorage.getItem('DeepSeek')) || [];
@@ -190,7 +191,7 @@ function displayChatList() {
         // Show full chat when clicked by loading its content into the textarea
         chatItem.addEventListener('click', () => {
             const inputField = document.getElementById('input');
-            inputField.value = chat.question + "\n\n" + chat.message;
+            inputField.value = chat.question + "\t\t model: " + modelLlm + "\n\n" + chat.message;
         });
 
         // Create delete icon
